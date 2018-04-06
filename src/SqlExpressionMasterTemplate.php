085c8e92e67c02cad4f2f470a7d1b281b1d43ef9
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-06 13:34:19 +0200

    Added master template class

---
+++ src/SqlExpressionMasterTemplate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RebelCode\Storage\Resource\WordPress\Module;
+
+use Dhii\Expression\Renderer\AbstractBaseSelfDelegateExpressionTemplate;
+use Psr\Container\ContainerInterface;
+
+/**
+ * A self-delegating master expression template.
+ *
+ * Given any template it delegates rendering to another template, retrieved by expression type from a container.
+ *
+ * @since [*next-version*]
+ */
+class SqlExpressionMasterTemplate extends AbstractBaseSelfDelegateExpressionTemplate
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param ContainerInterface $container The delegate container.
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->_setTermTypeRendererContainer($container);
+    }
+}
