1cb59e41a7a59ec4749a792ce686b2e040c3e990
Miguel Muscat <miguelmuscat93@gmail.com>
2018-12-21 10:21:36 +0100

    Moved services to service provider class

---
+++ src/Module/Module.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RebelCode\Storage\Resource\WordPress\Module;
+
+use Psr\Container\ContainerInterface;
+use RebelCode\Modular\Module\AbstractModule;
+
+/**
+ * WordPress CQRS module class.
+ *
+ * @since [*next-version*]
+ */
+class Module extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function run(ContainerInterface $c = null)
+    {
+    }
+}
